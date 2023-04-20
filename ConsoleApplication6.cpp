589f6a217d96760b2e5049f6a1c15ebdcc3845f1
Furqan Ali <77490589+Furqanali111@users.noreply.github.com>
2023-04-20 16:17:00 +0500

    Update ConsoleApplication6.cpp

--- ConsoleApplication6.cpp
+++ ConsoleApplication6.cpp
@@ -1,50 +1,85 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
+#include <stdlib.h>     /* atoi */
 
-void vulnerability1(char* input) {
-    char buffer[10];
-    strcpy(buffer, input);
-    printf("Buffer contents: %s\n", buffer);
+struct dataset {
+  char tempBuffer[2048];
+  char input[20];
+  int credits;
+};
+
+
+int getMenu(){
+  int value;
+  char input[2];
+  printf("\n--- Menu ---\n");
+  printf("[1] Peek for 50 Credits\n");
+  printf("[2] Poke for 100 Credits\n");
+  printf("[-1] Quit\n");
+  printf("> ");
+  scanf("%2s", &input);
+  value = atoi(input);
+  printf("Input %d \n", value);
+  return value;
 }
 
-void vulnerability2(char* input) {
-    char* buffer = (char*)malloc(strlen(input));
-    strcpy(buffer, input);
-    printf("Buffer contents: %s\n", buffer);
-    free(buffer);
+void peek(void){
+      int value;
+      char input[5];
+      char tmp[10];
+      printf("How far to peek > ");
+      scanf("%3s", input);
+      value = atoi(input);
+      sprintf(tmp, "%%%d$llx", value);
+      printf("%s\n", tmp);
+      printf("You Peek ahead and see ", tmp, "\n");
+      printf(tmp);
 }
 
-void vulnerability3() {
-    char buffer[10];
-    printf("Enter a password:\n");
-    scanf("%s", buffer);
-    if (strcmp(buffer, "password123") == 0) {
-        printf("Access granted!\n");
+int vuln(void){
+  //Important,  keep he stack in the correct sequence
+  struct dataset thedata={"", "",100};
+  int choice; //Menu Choice.
+  
+  while (thedata.credits > 0) {
+    printf("\nYou have %u Credits\n", thedata.credits);
+    printf("Data is '%s'\n", thedata.input);
+    choice = getMenu();
+    printf("Choice was %d\n", choice);
+    if (choice == 1){
+      thedata.credits -= 50;
+      peek();
+    }
+    else if (choice == 2){
+      thedata.credits -= 100;
+      printf("What to Poke >");
+      fflush(stdout);
+      read(0, thedata.tempBuffer, 2048);
+
+      //And copy
+      printf("Message Stored\n");
+      strcpy(thedata.input, thedata.tempBuffer);
+    }
+    else if (choice == -1){
+      printf("Quitting\n");
+      return 0;
     }
     else {
-        printf("Access denied.\n");
+      printf("You do Nothing\n");
     }
+
+  }
+  printf("\nNo Credits left.. Exiting\n");
+  return 0;
 }
 
-void vulnerability4() {
-    char buffer[10];
-    printf("Enter your name:\n");
-    gets(buffer);
-    printf("Hello, %s!\n", buffer);
-}
 
-void vulnerability5(char* input) {
-    char buffer[10];
-    snprintf(buffer, sizeof(buffer), "%s", input);
-    printf("Buffer contents: %s\n", buffer);
-}
-
-int main(int argc, char** argv) {
-    vulnerability1(argv[1]); // buffer overflow
-    vulnerability2(argv[1]); // heap overflow
-    vulnerability3();        // format string vulnerability
-    vulnerability4();        // buffer overflow
-    vulnerability5(argv[1]); // potential buffer overflow
-    return 0;
+int main(int argc, char* argv[]){
+  setbuf(stdout, NULL);
+  printf("1) Use the Source Luke \n");
+  printf("2) Everything is for a reason \n");
+  printf("3) Dissassemble./ PEDA / GEF..\n");
+  printf("4) Think about the stack!\n");
+  return vuln();
 }
